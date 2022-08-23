37c460a47421d461bfa029d33b6868ae0b870781
brayan-cincoe <brayanrcaicedo@gmail.com>
2022-08-22 20:17:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, File, Form, UploadFile
-#import matplotlib.pyplot as plt
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import cv2
@@ -7,21 +8,30 @@ import os
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    #allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 @app.post("/upload_photo")
-async def upload_photo(
-    file: UploadFile = File(...)
-    #first: str = Form(...),
-    #second: str = Form("default value")
-):
+async def upload_photo(file: UploadFile = File(...)):
     print("Starting...")
-    contents = await file.read()
-    nparr = np.fromstring(contents, np.uint8)
+    content = await file.read()
+    nparr = np.fromstring(content, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     resized_image = cv2.resize(img, (256, 256), interpolation = cv2.INTER_AREA)
     cv2.imwrite('image.jpg', resized_image)
 
     TEST_WEIGHTS = 'best.pt'
-    IMAGE_PATH   =  'image.jpg' # '../test_files/test2.jpeg'
+    IMAGE_PATH   =  'image.jpg' 
     
     #system("pip install -r yolov5/requirements.txt --user")
     #os.system(f"python yolov5/detect.py --source {TEST_IMAGE_PATH} --weights {TEST_WEIGHTS} --save-txt")
@@ -36,7 +46,4 @@ async def upload_photo(
     
     os.remove(IMAGE_PATH)
 
-    return {
-        "result": results.pandas().xyxy[0].to_json(),
-        #"type": file.content_type,
-    }
+    return { "result": results.pandas().xyxy[0].to_json() }
